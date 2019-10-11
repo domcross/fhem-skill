785a78002a7bb28ead6e966975b667f6e8d69554
domcross <dkreutz@t-online.de>
2019-10-11 15:16:25 +0200

    minimum test case

--- test/run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source $TRAVIS_BUILD_DIR/mycroft-core/.venv/bin/activate
-$TRAVIS_BUILD_DIR/mycroft-core/.venv/bin/python /opt/mycroft/skills/mycroft-homeassistant.btotharye/skill_developers_testrunner.py
